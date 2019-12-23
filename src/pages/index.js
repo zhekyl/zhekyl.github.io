75a65b9e88b6a14528958b18976cbf983e59b2b7
Kyle Zheng <kylezheng73@gmail.com>
2019-12-23 01:14:59 -0500

    css update and code refactor

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import indexStyles from "./index.module.css"
 import SEO from "../components/seo"
-import IndexHeader from "../components/IndexHeader"
+import PageHeader from "../components/PageHeader"
 import spin from "../images/spin.gif"
 import Layout from "../components/layout"
 
@@ -12,17 +12,17 @@ import Fade from 'react-reveal/Fade'
 const IndexPage = () => {
   return (
     <>
-    <IndexHeader></IndexHeader>
+    <PageHeader sections={["about", "projects", "resume"]}></PageHeader>
     <Layout>
       <SEO title="Home" />
       <div className={indexStyles.fullpage}>
         <div className={indexStyles.homeContent}>
-          <h1 className={indexStyles.name}>Kyle Zheng</h1>
+          <h1 id={indexStyles.name}>Kyle Zheng</h1>
           <div id={indexStyles.tagline}>
             <p>is looking for </p>
             <span className={indexStyles.rotating}>
               <span>a cool new project.</span>
-              <span>internship opportunities!</span>
+              <span>internships!</span>
               <span>love?</span>
             </span>
           </div>
@@ -30,7 +30,7 @@ const IndexPage = () => {
         <a className={indexStyles.bounce}>VowoV</a>
       </div>
       <Fade left>
-          <div className={indexStyles.section}>
+          <div className={indexStyles.thinSection}>
         <h1 id="about">About</h1>
         <h2>Education</h2>
         <p>
@@ -55,18 +55,27 @@ const IndexPage = () => {
       </div>
       </Fade>
       <Fade right>
-        <div className={indexStyles.section}>
+        <div className={indexStyles.wideSection}>
         <h1 id="projects">Projects</h1>
-        <p>KGG ? </p>
-        <p>Purdue Buildings</p>
-        <p>Grapevine</p>
-        <p>Mobile / Android / Unity / Game</p>
-        <p>Splodge, Rocket Run, Slyme, Stomp, Hacker Hero</p>
-        <p>this website lmao</p>
+          <div>
+            <h4>A Variety of Hypercasual Mobile Games</h4>
+            <p>Built with the Unity game engine</p>
+            <p>Splodge, Rocket Run, Slyme, Stomp, Hacker Hero</p>
+          </div>
+          <div>
+            <h4>Web Development</h4>
+            <p>Build using modern and responsive frameworks</p>
+            <p>KGG.wtf, kylezheng.tech</p>
+          </div>
+          <div>
+            <h4>Hackathon Projects</h4>
+            <p>Random crazy stuff</p>
+            <p>Purdue Buildings, Grapevine, Hacker Hero</p>
+          </div>
       </div>
       </Fade>
       <Fade left>
-        <div className={indexStyles.section}>
+        <div className={indexStyles.thinSection}>
         <h1 id="resume">Resume</h1>
         <p>
           If I haven't scared you away yet, take a look at my resume!
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,11 +2,9 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import IndexHeader from "../components/IndexHeader"
 
 const ProjectsPage = () => (
   <>
-    <IndexHeader></IndexHeader>
     <Layout>
       <SEO title="Test projects page" />
       <h1>lmao</h1>
